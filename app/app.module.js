0f49407e1b53ac878a86db4a530794d8e204901b
fisherjrs <fisher.j.r.s@gmail.com>
2015-09-04 15:49:32 -0500

    new html pages

--- app/app.module.js
+++ app/app.module.js
@@ -4,9 +4,22 @@ app.controller('DesignController', function($scope, $http) {
     name: "Richard Hawley",
     occupation: "Star Gazer"
   };
+  $scope.mode = 'offline';
+  $scope.designId = 9;
   $scope.add = function(amount) { $scope.person.occupation = amount; };
   $scope.hello = function(amount) {
   	$http.get('http://localhost:9000/conduitservices/getdesigndefinition.json?designId=' + amount).
+        success(function(data) {
+            $scope.person.occupation = data.categoryDefinition[0].categoryName;
+            $scope.categories = data.categoryDefinition;
+        });
+  };
+  $scope.getCategories = function(designId) {
+    url = 'http://localhost:9000/conduitservices/getdesigndefinition.json?designId=';
+    if ($scope.mode == 'offline') {
+        url = 'http://localhost:9000/conduitservices/getdesigndefinitionoffline.json?designId='
+    }
+    $http.get( url + designId).
         success(function(data) {
             $scope.person.occupation = data.categoryDefinition[0].categoryName;
             $scope.categories = data.categoryDefinition;
